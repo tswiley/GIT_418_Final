bb44ef8489757516733aef8d29f773af05abad48
Travis Wiley <traviswiley88@gmail.com>
2023-12-01 11:55:55 -0600

    changes to html, css and js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-"use strict";
+$(document).ready(function () {
+    $('.speakers-carousel').slick({
+      infinite: true,
+      slidesToShow: 3, // Adjust the number of speakers shown at once
+      slidesToScroll: 1,
+      prevArrow: '<button type="button" class="slick-prev">Previous</button>',
+      nextArrow: '<button type="button" class="slick-next">Next</button>',
+    });
+  });
