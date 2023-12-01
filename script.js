ad94291c53ca6885738e2d153a01b529ce3bde07
Travis Wiley <traviswiley88@gmail.com>
2023-12-01 14:57:12 -0600

    changes to html structure and css

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-$(document).ready(function () {
-    $('.speakers-carousel').slick({
-      infinite: true,
-      slidesToShow: 3, // Adjust the number of speakers shown at once
-      slidesToScroll: 1,
-      prevArrow: '<button type="button" class="slick-prev">Previous</button>',
-      nextArrow: '<button type="button" class="slick-next">Next</button>',
+// script.js
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Code for handling tabs
+
+    // Schedule Tabs
+    $('.tabs button').on('click', function () {
+        const target = $(this).data('target');
+        $('.tab-content > div').removeClass('active');
+        $(`#${target}`).addClass('active');
     });
-  });
+
+    // Sponsors Tabs
+    $('.sponsor-tabs button').on('click', function () {
+        const target = $(this).data('target');
+        $('.sponsor-content > div').removeClass('active');
+        $(`#${target}`).addClass('active');
+    });
+
+    // Code for other functionality, if any
+});
